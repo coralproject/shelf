5683f3483a8cff2f9f80aa265f1e6702f4228ed9
Ewetumo Alexander <trinoxf@gmail.com>
2015-11-23 17:48:34 +0100

    fixing io package

--- pkg/db/query/service.go
+++ pkg/db/query/service.go
@@ -1,6 +1,11 @@
 package query
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/coralproject/shelf/pkg/db/mongo"
@@ -103,4 +108,130 @@ func RemoveQuerySet(context interface{}, ses *mgo.Session, name string) (*QueryS
 
 	log.Dev(context, "RemoveQuerySet", "Completed")
 	return &rs, nil
+}
+
+// RuleSetFromReader serializes the content of a RuleSet from a io.Reader.
+// Returns the serialized RuleSet pointer, else returns a non-nil error if
+// the operation failed.
+func RuleSetFromReader(context interface{}, r io.Reader) (*QuerySet, error) {
+	log.Dev(context, "RuleSetFromReader", "Started : Load RuleSet")
+	var rs QuerySet
+
+	err := json.NewDecoder(r).Decode(&rs)
+	if err != nil {
+		log.Error(context, "RuleSetFromReader", err, "Completed : Load RuleSet")
+		return nil, err
+	}
+
+	log.Dev(context, "RuleSetFromReader", "Completed : Load RuleSet")
+	return &rs, nil
+}
+
+// RuleSetFromFile serializes the content of a RuleSet from a file using the
+// given file path.
+// Returns the serialized query.RuleSet, else returns a non-nil error if
+// the operation failed.
+func RuleSetFromFile(context interface{}, path string) (*QuerySet, error) {
+	log.Dev(context, "RuleSetFromFile", "Started : Load RuleSet : File %s", path)
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Error(context, "RuleSetFromFile", err, "Completed : Load RuleSet : File %s", path)
+		return nil, err
+	}
+
+	var rs QuerySet
+
+	err = json.NewDecoder(file).Decode(&rs)
+	if err != nil {
+		log.Error(context, "RuleSetFromFile", err, "Completed : Load RuleSet : File %s", path)
+		return nil, err
+	}
+
+	log.Dev(context, "RuleSetFromFile", "Completed : Load RuleSet : File %s", path)
+	return &rs, nil
+}
+
+// rulesFromPaths loads sets of rules from the giving array of file paths.
+// Returns a list of query.Rule, each serialized with the contents of it's file.
+// If any of the paths are invalid or there was a failure to load their content,
+// a non-nil error is returned.
+func rulesFromPaths(context interface{}, ruleFilePaths []string) ([]Query, error) {
+	log.Dev(context, "RuleFromPaths", "Started : Paths %s", ruleFilePaths)
+
+	var rules []Query
+
+	for _, file := range ruleFilePaths {
+		ruleFile, err := os.Open(file)
+		if err != nil {
+			log.Error(context, "RuleFromPaths", err, "Completed : Paths %s", ruleFilePaths)
+			return nil, err
+		}
+
+		var r Query
+		err = json.NewDecoder(ruleFile).Decode(&r)
+		if err != nil {
+			log.Error(context, "RuleFromPaths", err, "Completed : Paths %s", ruleFilePaths)
+			return nil, err
+		}
+
+		rules = append(rules, r)
+	}
+
+	log.Dev(context, "RuleFromPaths", "Completed : Paths %s", ruleFilePaths)
+	return rules, nil
+}
+
+// rulesFromDir loads sets of rules from the giving files in the directory path,
+// only reading the current directory level and not sub-directories.
+// Returns a list of Rule pointers, each serialized with the contents of it's file.
+// If any of the paths are invalid or there was a failure to load their content,
+// a non-nil error is returned.
+func rulesFromDir(context interface{}, dirPath string) ([]Query, error) {
+	log.Dev(context, "RulesFromDir", "Started : Load Rules : Dir %s", dirPath)
+
+	stat, err := os.Stat(dirPath)
+	if err != nil {
+		log.Error(context, "RulesFromDir", err, "Completed : Load Rules : Dir %s", dirPath)
+		return nil, err
+	}
+
+	if !stat.IsDir() {
+		log.Error(context, "RulesFromDir", fmt.Errorf("Path[%s] is not a Directory", dirPath), "Completed : Load Rules : Dir %s", dirPath)
+		return nil, err
+	}
+
+	//open up the filepath since its a directory, read and sort
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		log.Error(context, "RulesFromDir", err, "Completed : Load Rules : Dir %s", dirPath)
+		return nil, err
+	}
+
+	filesInfo, err := dir.Readdir(0)
+	if err != nil {
+		log.Error(context, "RulesFromDir", err, "Completed : Load Rules : Dir %s", dirPath)
+		return nil, err
+	}
+
+	dir.Close()
+
+	var files []string
+
+	for _, info := range filesInfo {
+		if info.IsDir() {
+			continue
+		}
+
+		files = append(files, filepath.Join(dirPath, info.Name()))
+	}
+
+	rules, err := RulesFromPaths(context, files)
+	if err != nil {
+		log.Error(context, "RulesFromDir", err, "Completed : Load Rules : Dir %s", dirPath)
+		return nil, err
+	}
+
+	log.Dev(context, "RulesFromDir", "Completed : Load Rules : Dir %s", dirPath)
+	return rules, nil
 }
