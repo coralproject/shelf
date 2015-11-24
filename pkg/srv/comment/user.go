71790f931e19a28bace6bcc823ff1bc9afa79f76
samshub <samaresh.panda@washpost.com>
2015-11-24 11:39:55 -0500

    Updated all files based on feedback from golint.

--- pkg/srv/comment/user.go
+++ pkg/srv/comment/user.go
@@ -2,22 +2,21 @@ package comment
 
 import "time"
 
+//User denotes a user in the system
 type User struct {
-	Id       		string 		`json:"id" bson:"_id"`
-	Email    		string 		`json:"email" bson:"email"`
-	Password 		string 		`json:"password" bson:"password"`
-	DisplayName     string 		`json:"displayName" bson:"displayName"`
-	Avatar    		string 		`json:"avatar" bson:"avatar"`
-	Name     		string 		`json:"name" bson:"name"`
-	Address  		string 		`json:"address" bson:"address"`
-	City  			string 		`json:"city" bson:"city"`
-	State  			string 		`json:"state" bson:"state"`
-	ZipCode  		string 		`json:"zipCode" bson:"zipCode"`
-	Gender   		string 		`json:"gender" bson:"gender"`
-	AgeGroup 		string 		`json:"ageGroup" bson:"ageGroup"`
-	LastLogin 		time.Time 	`json:"lastLogin" bson:"lastLogin"`
-	MemberSince     time.Time 	`json:"memberSince" bson:"memberSince"`
-	TrustScore 		int 		`json:"trustScore" bson:"trustScore"`
+	ID          string    `json:"id" bson:"_id"`
+	Email       string    `json:"email" bson:"email"`
+	Password    string    `json:"password" bson:"password"`
+	DisplayName string    `json:"displayName" bson:"displayName"`
+	Avatar      string    `json:"avatar" bson:"avatar"`
+	Name        string    `json:"name" bson:"name"`
+	Address     string    `json:"address" bson:"address"`
+	City        string    `json:"city" bson:"city"`
+	State       string    `json:"state" bson:"state"`
+	ZipCode     string    `json:"zipCode" bson:"zipCode"`
+	Gender      string    `json:"gender" bson:"gender"`
+	AgeGroup    string    `json:"ageGroup" bson:"ageGroup"`
+	LastLogin   time.Time `json:"lastLogin" bson:"lastLogin"`
+	MemberSince time.Time `json:"memberSince" bson:"memberSince"`
+	TrustScore  int       `json:"trustScore" bson:"trustScore"`
 }
-
-type UserArray []User
