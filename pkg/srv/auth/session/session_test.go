69630e834fe6cfcff99ceb9df1538dd0e2458310
William Kennedy <bill@ardanlans.com>
2015-11-24 11:37:11 -0500

    Saving test to figure out why verbose flag is not being respected.

--- pkg/srv/auth/session/session_test.go
+++ pkg/srv/auth/session/session_test.go
@@ -1,30 +1,32 @@
 package session_test
 
 import (
-	"encoding/json"
-	"os"
+	"fmt"
 	"testing"
 	"time"
-
-	"gopkg.in/mgo.v2"
 
 	"github.com/coralproject/shelf/pkg/srv/auth/session"
 	"github.com/coralproject/shelf/pkg/srv/mongo"
 	"github.com/coralproject/shelf/pkg/tests"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var (
 	collection = "sessions"
 	publicID   = "6dcda2da-92c3-11e5-8994-feff819cdc9f"
+	context    = "testing"
 )
 
 func init() {
+	fmt.Println("*****>", testing.Verbose())
 	tests.Init()
 }
 
 // removeSessions is used to clear out all the test sessions that are
 // created from tests.
-func removeSessions() error {
+func removeSessions(ses *mgo.Session) error {
 	f := func(c *mgo.Collection) error {
 		q := bson.M{"public_id": publicID}
 		return c.Remove(q)
@@ -35,7 +37,7 @@ func removeSessions() error {
 
 // retrieveSession is used to validate sessions are being saved
 // correctly.
-func retrieveSession(sessionID string) (*session.Session, error) {
+func retrieveSession(ses *mgo.Session, sessionID string) (*session.Session, error) {
 	var s session.Session
 	f := func(c *mgo.Collection) error {
 		q := bson.M{"session_id": sessionID}
@@ -55,36 +57,36 @@ func TestCreate(t *testing.T) {
 	{
 		t.Logf("\tWhen using PublicID %s", publicID)
 		{
-			if err := removeSessions(); err != nil {
-				t.Fatalf("\t%s\tShould be able to remove all sessions.", tests.Failed)
-			}
-			t.Logf("\t%s\tShould be able to remove all sessions.", tests.Succeed)
-
 			ses := mongo.GetSession()
 			defer ses.Close()
 
-			s1, err := session.Create("context", ses, publicID, 10*time.Second)
+			if err := removeSessions(ses); err != nil {
+				t.Fatalf("\t%s\tShould be able to remove all sessions.", tests.Failed)
+			}
+			t.Logf("\t%s\tShould be able to remove all sessions.", tests.Success)
+
+			s1, err := session.Create(context, ses, publicID, 10*time.Second)
 			if err != nil {
 				t.Fatalf("\t%s\tShould be able to create a session.", tests.Failed)
 			}
-			t.Logf("\t%s\tShould be able to create a session.", tests.Succeed)
+			t.Logf("\t%s\tShould be able to create a session.", tests.Success)
 
-			s2, err := retrieveSession(s1.SessionID)
+			s2, err := retrieveSession(ses, s1.SessionID)
 			if err != nil {
 				t.Fatalf("\t%s\tShould be able to retrieve the session.", tests.Failed)
 			}
-			t.Logf("\t%s\tShould be able to retrieve the session.", tests.Succeed)
+			t.Logf("\t%s\tShould be able to retrieve the session.", tests.Success)
 
 			if s1.SessionID != s2.SessionID {
 				t.Fatalf("\t%s\tShould be able to get back the same session.", tests.Failed)
 			} else {
-				t.Logf("\t%s\tShould be able to get back the same session.", tests.Succeed)
+				t.Logf("\t%s\tShould be able to get back the same session.", tests.Success)
 			}
 
 			if s1.PublicID != s2.PublicID {
 				t.Fatalf("\t%s\tShould be able to get back the same user.", tests.Failed)
 			} else {
-				t.Logf("\t%s\tShould be able to get back the same user.", tests.Succeed)
+				t.Logf("\t%s\tShould be able to get back the same user.", tests.Success)
 			}
 		}
 	}
