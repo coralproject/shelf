107483a60a5468e22647a1ee9a94000a3b427ea0
Ewetumo Alexander <trinoxf@gmail.com>
2015-12-01 15:18:14 +0100

    updating shelf cli testing base

--- pkg/srv/query/query_test.go
+++ pkg/srv/query/query_test.go
@@ -371,22 +371,22 @@ func TestDeleteSet(t *testing.T) {
 		t.Log("\tWhen using fixture", fixture)
 		{
 			if err := query.CreateSet(context, ses, qs1); err != nil {
-				t.Errorf("\t%s\tShould be able to create a query set : %s", tests.Failed, err)
+				t.Fatalf("\t%s\tShould be able to create a query set : %s", tests.Failed, err)
 			}
 			t.Logf("\t%s\tShould be able to create a query set.", tests.Success)
 
 			if err := query.DeleteSet(context, ses, qsName); err != nil {
-				t.Errorf("\t%s\tShould be able to delete a query set using its name[%s]: %s", tests.Failed, qsName, err)
+				t.Fatalf("\t%s\tShould be able to delete a query set using its name[%s]: %s", tests.Failed, qsName, err)
 			}
 			t.Logf("\t%s\tShould be able to delete a query set using its name[%s]:", tests.Success, qsName)
 
 			if err := query.DeleteSet(context, ses, qsBadName); err == nil {
-				t.Errorf("\t%s\tShould not be able to delete a query set using wrong name name[%s]", tests.Failed, qsBadName)
+				t.Fatalf("\t%s\tShould not be able to delete a query set using wrong name name[%s]", tests.Failed, qsBadName)
 			}
 			t.Logf("\t%s\tShould not be able to delete a query set using wrong name name[%s]", tests.Success, qsBadName)
 
 			if _, err := query.GetSetByName(context, ses, qsName); err == nil {
-				t.Errorf("\t%s\tShould be able to validate query set with Name[%s] does not exists: %s", tests.Failed, qsName, errors.New("Record Exists"))
+				t.Fatalf("\t%s\tShould be able to validate query set with Name[%s] does not exists: %s", tests.Failed, qsName, errors.New("Record Exists"))
 			}
 			t.Logf("\t%s\tShould be able to validate query set with Name[%s] does not exists:", tests.Success, qsName)
 		}
@@ -459,34 +459,30 @@ func TestAPIFailure(t *testing.T) {
 		t.Log("When giving a nil session")
 		{
 			if err := query.CreateSet(context, nil, qs1); err == nil {
-				t.Errorf("\t%s\tShould be refused create by api with bad session", tests.Failed)
-			} else {
-				t.Logf("\t%s\tShould be refused create by api with bad session: %s", tests.Success, err)
+				t.Fatalf("\t%s\tShould be refused create by api with bad session", tests.Failed)
 			}
+			t.Logf("\t%s\tShould be refused create by api with bad session: %s", tests.Success, err)
 
 			if err := query.UpdateSet(context, nil, qs1); err == nil {
-				t.Errorf("\t%s\tShould be refused update by api with bad session", tests.Failed)
-			} else {
-				t.Logf("\t%s\tShould be refused update by api with bad session: %s", tests.Success, err)
+				t.Fatalf("\t%s\tShould be refused update by api with bad session", tests.Failed)
 			}
+			t.Logf("\t%s\tShould be refused update by api with bad session: %s", tests.Success, err)
 
 			if _, err := query.GetSetByName(context, nil, qsName); err == nil {
-				t.Errorf("\t%s\tShould be refused get request by api with bad session", tests.Failed)
-			} else {
-				t.Logf("\t%s\tShould be refused get request by api with bad session: %s", tests.Success, err)
+				t.Fatalf("\t%s\tShould be refused get request by api with bad session", tests.Failed)
 			}
+			t.Logf("\t%s\tShould be refused get request by api with bad session: %s", tests.Success, err)
 
 			if _, err := query.GetSetNames(context, nil); err == nil {
-				t.Errorf("\t%s\tShould be refused names request by api with bad session", tests.Failed)
-			} else {
-				t.Logf("\t%s\tShould be refused names request by api with bad session: %s", tests.Success, err)
+				t.Fatalf("\t%s\tShould be refused names request by api with bad session", tests.Failed)
 			}
+			t.Logf("\t%s\tShould be refused names request by api with bad session: %s", tests.Success, err)
 
 			if err := query.DeleteSet(context, nil, qsName); err == nil {
-				t.Errorf("\t%s\tShould be refused delete by api with bad session", tests.Failed)
-			} else {
-				t.Logf("\t%s\tShould be refused delete by api with bad session: %s", tests.Success, err)
+				t.Fatalf("\t%s\tShould be refused delete by api with bad session", tests.Failed)
 			}
+			t.Logf("\t%s\tShould be refused delete by api with bad session: %s", tests.Success, err)
+
 		}
 	}
 }
