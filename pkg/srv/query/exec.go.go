873052f82d6333d7d655c176b546405a7487a86d
Ewetumo Alexander <trinoxf@gmail.com>
2015-12-02 15:45:31 +0100

    revised exec code and test

--- pkg/srv/query/exec.go.go
+++ pkg/srv/query/exec.go.go
@@ -11,6 +11,11 @@ import (
 // used in matching against a script src.
 var variableMarkersRegExp = regexp.MustCompile(`#(.*?)#`)
 
+// RenderResult converts a bson.M response into a map of key values pairs.
+func RenderResult(data bson.M, options VarOption) (map[string]string, error) {
+	return nil, nil
+}
+
 // renderScript evalues and replaces all variable markers (#variable#) in the
 // source script string with the approrpiate value from the map if found.
 func renderScript(script string, variables map[string]string) string {
@@ -31,9 +36,4 @@ func renderScript(script string, variables map[string]string) string {
 	}
 
 	return script
-}
-
-// RenderResult converts a bson.M response into a map of key values pairs.
-func RenderResult(data bson.M, options VarOption) (map[string]string, error) {
-	return nil, nil
 }
