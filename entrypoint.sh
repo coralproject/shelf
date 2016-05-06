405eb42ac0d550a73d98227e21db9893fab0da47
MixedMessages <angel.santiago4096@gmail.com>
2016-05-06 09:31:57 -0400

    Slight renaming

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,15 @@ DEFAULT_XENIA_COMMAND_PATH=/go/src/github.com/coralproject/xenia/cmd/xenia
 PATH=/go/bin:${PATH}
 
 # Create the database collections if environment variable XENIA_CREATE_DATABASE is present.
-# XENIA_DATABASE_JSON - the location of the scrdb json.
+# XENIA_DATABASE_SCHEMA_PATH - the location of the scrdb json.
 if [ ! -z ${XENIA_CREATE_DATABASE:-} ]; then
-    xenia db create -f ${XENIA_DATABASE_JSON:-${DEFAULT_XENIA_COMMAND_PATH}/scrdb/database.json}
+    xenia db create -f ${XENIA_DATABASE_SCHEMA_PATH:-${DEFAULT_XENIA_COMMAND_PATH}/scrdb/database.json}
 fi  
 
 # Update queries if the environment variable XENIA_UPDATE_QUERY is present.
-# XENIA_QUERY_JSON - the location of the scrquery directory.
+# XENIA_QUERY_UPSERT_PATH - the location of the scrquery directory or individual query file.
 if [ ! -z ${XENIA_UPDATE_QUERY:-} ]; then
-    xenia query upsert -p ${XENIA_QUERY_JSON:-${DEFAULT_XENIA_COMMAND_PATH}/scrquery}
+    xenia query upsert -p ${XENIA_QUERY_UPSERT_PATH:-${DEFAULT_XENIA_COMMAND_PATH}/scrquery}
 fi
 
 # Run the Xenia daemon last and always to maintain backwards compatibility with previous Dockerfile version.
