4a09a7fcf30b478547c2e4c0c2e08a306bea3556
Daniel Whitenack <whitenack.daniel@gmail.com>
2016-08-02 11:35:25 -0500

    Relationship and view validation

--- internal/shelf/model.go
+++ internal/shelf/model.go
@@ -1,9 +1,6 @@
 package shelf
 
-import (
-	"github.com/cayleygraph/cayley/graph/path"
-	validator "gopkg.in/bluesuncorp/validator.v8"
-)
+import validator "gopkg.in/bluesuncorp/validator.v8"
 
 //==============================================================================
 
@@ -16,8 +13,37 @@ func init() {
 
 //==============================================================================
 
+// RelManager contains metadata about what relationships and views are currenlty
+// being utilized in the system.
+type RelManager struct {
+	Relationships []Relationship `bson:"relationships" json:"relationships" validate:"required,min=1"`
+	Views         []View         `bson:"views" json:"views" validate:"required,min=1"`
+}
+
+// Validate checks the RelManager value for consistency.
+func (rm RelManger) Validate() error {
+
+	if err := validate.Struct(rm); err != nil {
+		return err
+	}
+
+	for _, rel := range rm.Relationships {
+		if err := rel.Validate(); err != nil {
+			return err
+		}
+	}
+	for _, view := range rm.Views {
+		if err := view.Validate(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // Relationship contains metadata about a relationship.
+// Note, predicate should be unique.
 type Relationship struct {
+	ID          string `bson:"id" json:"id" validate:"required, min=1"`
 	SubjectType string `bson:"subject" json:"subject" validate:"required,min=3"`
 	Predicate   string `bson:"predicate" json:"predicate" validate:"required,min=3"`
 	ObjectType  string `bson:"object" json:"object" validate:"required,min=3"`
@@ -36,9 +62,18 @@ func (r Relationship) Validate() error {
 
 // View contains metadata about a view.
 type View struct {
-	Name          string         `bson:"name" json:"name" validate:"required,min=3"`
-	Relationships []Relationship `bson:"relationships" json:"relationships"`
-	Path          path.Path      `bson:"path" json:"path"`
+	ID        string        `bson:"id" json:"id" validate:"required,min=1"`
+	Name      string        `bson:"name" json:"name" validate:"required,min=3"`
+	StartType string        `bson:"start_type" json:"start_type" validate:"required,min=3"`
+	Path      []PathSegment `bson:"path" json:"path" validate:"required,min=1"`
+}
+
+// PathSegment contains metadata about a segment of a path,
+// which path partially defines a View.
+type PathSegment struct {
+	Direction      string `bson:"direction" json:"direction" validate:"required,min=2"`
+	RelationshipID string `bson:"relationship_id" json:"relationship_id" validate:"required, min=1"`
+	Tag            string `bson:"tag,omitempty" json:"tag,omitempty"`
 }
 
 // Validate checks the View value for consistency.
@@ -46,6 +81,12 @@ func (v View) Validate() error {
 
 	if err := validate.Struct(v); err != nil {
 		return err
+	}
+
+	for _, segment := range v.PathSegment {
+		if err := segment.Validate(); err != nil {
+			return err
+		}
 	}
 	return nil
 }
