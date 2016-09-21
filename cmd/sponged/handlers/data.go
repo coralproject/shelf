ccac97ad967b4bc457afcea15a08c0fb786d73c8
David Erwin <davidjerwin@gmail.com>
2016-09-21 10:03:55 -0400

    Implement data import route

---
+++ cmd/sponged/handlers/data.go
@@ -0,0 +1,46 @@
+// Package handlers contains the handler logic for processing requests.
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/ardanlabs/kit/db"
+	"github.com/ardanlabs/kit/web/app"
+	"github.com/coralproject/shelf/internal/sponge/data"
+	"github.com/coralproject/shelf/internal/sponge/item"
+)
+
+// dataHandle maintains the set of handlers for the data api.
+type dataHandle struct{}
+
+// Data fronts the access to the data service functionality.
+var Data dataHandle
+
+//==============================================================================
+
+// Upsert receives POSTed data, itemizes it then Upserts it via the item service
+// 204 SuccessNoContent, 400 Bad Request, 404 Not Found, 500 Internal
+func (dataHandle) Upsert(c *app.Context) error {
+	var da data.Data
+	if err := json.NewDecoder(c.Request.Body).Decode(&da); err != nil {
+		return err
+	}
+
+	ty := c.Params["type"]
+
+	// Pass the version from the type config for now. TODO: Possibly add version
+	// option in a separate endpoint in the future.
+	var it item.Item
+	it, err := data.Itemize(c.SessionID, c.Ctx["DB"].(*db.DB), ty, 1, da)
+	if err != nil {
+		return err
+	}
+
+	if err := item.Upsert(c.SessionID, c.Ctx["DB"].(*db.DB), &it); err != nil {
+		return err
+	}
+
+	c.Respond(it, http.StatusNoContent)
+	return nil
+}
--- cmd/sponged/routes/routes.go
+++ cmd/sponged/routes/routes.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/anvilresearch/go-anvil"
 	"github.com/ardanlabs/kit/cfg"
 	"github.com/ardanlabs/kit/db"
 	"github.com/ardanlabs/kit/db/mongo"
@@ -56,20 +55,7 @@ func init() {
 // API returns a handler for a set of routes.
 func API() http.Handler {
 
-	// If authentication is on then configure Anvil.
-	var anv *anvil.Anvil
-	if url, err := cfg.String(cfgAnvilHost); err == nil {
-
-		log.Dev("startup", "Init", "Initalizing Anvil")
-		anv, err = anvil.New(url)
-		if err != nil {
-			log.Error("startup", "Init", err, "Initializing Anvil: %s", url)
-			os.Exit(1)
-		}
-	}
-
 	a := app.New(midware.Mongo, midware.Auth)
-	a.Ctx["anvil"] = anv
 
 	log.Dev("startup", "Init", "Initalizing routes")
 	routes(a)
@@ -83,6 +69,8 @@ func API() http.Handler {
 // routes manages the handling of the API endpoints.
 func routes(a *app.App) {
 	a.Handle("GET", "/1.0/version", handlers.Version.List)
+
+	a.Handle("POST", "/1.0/data/type/:type", handlers.Data.Upsert)
 
 	a.Handle("GET", "/1.0/item/:id", handlers.Item.Retrieve)
 	a.Handle("PUT", "/1.0/item", handlers.Item.Upsert)
