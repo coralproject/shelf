8e3a60cc296b46192da625a767a25cc2da7dc30f
David Erwin <davidjerwin@gmail.com>
2016-09-22 17:36:57 -0400

    Load and compare tests

--- cmd/sponged/tests/data_test.go
+++ cmd/sponged/tests/data_test.go
@@ -4,10 +4,13 @@ package tests
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/ardanlabs/kit/tests"
+
+	"github.com/coralproject/shelf/internal/sponge/item"
 	"github.com/coralproject/shelf/internal/sponge/item/itemfix"
 )
 
@@ -49,6 +52,37 @@ func DataUpsertItem(t *testing.T) {
 				t.Fatalf("\t%s\tShould be able to insert the data : %v", tests.Failed, w.Code)
 			}
 			t.Logf("\t%s\tShould be able to insert the data.", tests.Success)
+		}
+
+		//----------------------------------------------------------------------
+		// Retrieve the item.
+
+		var items []item.Item
+		url = "/1.0/item/" + fmt.Sprintf("%s_%v", typ, dat["id"])
+		r = tests.NewRequest("GET", url, nil)
+		w = httptest.NewRecorder()
+
+		a.ServeHTTP(w, r)
+
+		t.Logf("\tWhen calling url to get : %s", url)
+		{
+			if w.Code != 200 {
+				t.Fatalf("\t%s\tShould be able to retrieve the item : %v", tests.Failed, w.Code)
+			}
+			t.Logf("\t%s\tShould be able to retrieve the item.", tests.Success)
+
+			var itemsBack []item.Item
+			if err := json.Unmarshal(w.Body.Bytes(), &itemsBack); err != nil {
+				t.Fatalf("\t%s\tShould be able to unmarshal the results : %v", tests.Failed, err)
+			}
+			t.Logf("\t%s\tShould be able to unmarshal the results.", tests.Success)
+
+			if itemsBack[0].ID != items[0].ID || itemsBack[0].Type != items[0].Type {
+				t.Logf("\t%+v", items[0])
+				t.Logf("\t%+v", itemsBack[0])
+				t.Fatalf("\t%s\tShould be able to get back the same item.", tests.Failed)
+			}
+			t.Logf("\t%s\tShould be able to get back the same item.", tests.Success)
 		}
 	}
 }
