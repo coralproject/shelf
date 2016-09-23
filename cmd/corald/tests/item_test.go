6c6872b24c24bc34bea95002221dcd8886397e4b
gaba <gabelula@gmail.com>
2016-09-23 15:05:06 -0700

    Removes tests not being used.

--- cmd/corald/tests/item_test.go
+++ cmd/corald/tests/item_test.go
@@ -27,7 +27,7 @@ func TestItemsGET(t *testing.T) {
 		{
 			t.Log("\tWhen we user version 1.0 of the items endpoint.")
 			if w.Code != http.StatusOK {
-				t.Fatalf("\t%s\tShould be able to retrieve all the items for the query set High_quality_commenters_in_Politics.", tests.Failed)
+				t.Fatalf("\t%s\tShould be able to retrieve all the items for the query set High_quality_commenters_in_Politics: %v", tests.Failed, w.Code)
 			}
 			t.Logf("\t%s\tShould be able to retrieve the items for the query set.", tests.Success)
 
@@ -61,73 +61,6 @@ func TestItemsGET(t *testing.T) {
 			} else {
 				t.Logf("\t%s\tShould have the correct id.", tests.Success)
 			}
-		}
-	}
-}
-
-// // TestItemPOST sample test for the POST call.
-// func TestItemPOST(t *testing.T) {
-// 	tests.ResetLog()
-// 	defer tests.DisplayLog()
-//
-// 	t.Log("Given the need to test POST form call.")
-// 	{
-// 		url := "/v1/item"
-// 		r := tests.NewRequest("POST", url, nil)
-// 		w := httptest.NewRecorder()
-//
-// 		a.ServeHTTP(w, r)
-//
-// 		t.Logf("\tWhen calling url : %s", url)
-// 		{
-// 			t.Log("\tWhen we user version 1.0 of the forms endpoint.")
-// 			if w.Code != http.StatusCreated {
-// 				t.Fatalf("\t%s\tShould be able to retrieve the forms list : %v", tests.Failed, w.Code)
-// 			}
-// 			t.Logf("\t%s\tShould be able to retrieve the forms list.", tests.Success)
-//
-// 			var form struct {
-// 				ID string `json:"id"`
-// 			}
-//
-// 			err := json.NewDecoder(w.Body).Decode(&form)
-// 			if err != nil {
-// 				t.Fatalf("\t%s\tShould be able to unmarshal the response : %v", tests.Failed, err)
-// 			}
-// 			t.Logf("\t%s\tShould be able to unmarshal the response.", tests.Success)
-//
-// 			want := "57daa6680cefe53d4b2adce0"
-// 			if form.ID != want {
-// 				t.Log("GOT :", form.ID)
-// 				t.Log("WANT:", want)
-// 				t.Errorf("\t%s\tShould have the correct id.", tests.Failed)
-// 			} else {
-// 				t.Logf("\t%s\tShould have the correct id.", tests.Success)
-// 			}
-// 		}
-// 	}
-// }
-
-// TestItemsPUT sample test for the PUT call.
-func TestItemsPUT(t *testing.T) {
-	tests.ResetLog()
-	defer tests.DisplayLog()
-
-	t.Log("Given the need to test POST form call.")
-	{
-		url := "/v1/item/1"
-		r := tests.NewRequest("PUT", url, nil)
-		w := httptest.NewRecorder()
-
-		a.ServeHTTP(w, r)
-
-		t.Logf("\tWhen calling url : %s", url)
-		{
-			t.Log("\tWhen we user version 1.0 of the forms endpoint.")
-			if w.Code != http.StatusNoContent {
-				t.Fatalf("\t%s\tShould be able to retrieve the forms list : %v", tests.Failed, w.Code)
-			}
-			t.Logf("\t%s\tShould be able to retrieve the forms list.", tests.Success)
 		}
 	}
 }
