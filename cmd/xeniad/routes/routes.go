02c2819ebf3d9205279bd93433ad2d672cc86281
gaba <gabelula@gmail.com>
2016-09-23 15:06:10 -0700

    Moves Xeniad endpoints from 1.0 to v1 format.

--- cmd/xeniad/routes/routes.go
+++ cmd/xeniad/routes/routes.go
@@ -91,48 +91,48 @@ func API(testing ...bool) http.Handler {
 
 // routes manages the handling of the API endpoints.
 func routes(a *app.App) {
-	a.Handle("GET", "/1.0/version", handlers.Version.List)
+	a.Handle("GET", "/v1/version", handlers.Version.List)
 
-	a.Handle("GET", "/1.0/script", handlers.Script.List)
-	a.Handle("PUT", "/1.0/script", handlers.Script.Upsert)
-	a.Handle("GET", "/1.0/script/:name", handlers.Script.Retrieve)
-	a.Handle("DELETE", "/1.0/script/:name", handlers.Script.Delete)
+	a.Handle("GET", "/v1/script", handlers.Script.List)
+	a.Handle("PUT", "/v1/script", handlers.Script.Upsert)
+	a.Handle("GET", "/v1/script/:name", handlers.Script.Retrieve)
+	a.Handle("DELETE", "/v1/script/:name", handlers.Script.Delete)
 
-	a.Handle("GET", "/1.0/query", handlers.Query.List)
-	a.Handle("PUT", "/1.0/query", handlers.Query.Upsert)
-	a.Handle("GET", "/1.0/query/:name", handlers.Query.Retrieve)
-	a.Handle("DELETE", "/1.0/query/:name", handlers.Query.Delete)
+	a.Handle("GET", "/v1/query", handlers.Query.List)
+	a.Handle("PUT", "/v1/query", handlers.Query.Upsert)
+	a.Handle("GET", "/v1/query/:name", handlers.Query.Retrieve)
+	a.Handle("DELETE", "/v1/query/:name", handlers.Query.Delete)
 
-	a.Handle("PUT", "/1.0/index/:name", handlers.Query.EnsureIndexes)
+	a.Handle("PUT", "/v1/index/:name", handlers.Query.EnsureIndexes)
 
-	a.Handle("GET", "/1.0/regex", handlers.Regex.List)
-	a.Handle("PUT", "/1.0/regex", handlers.Regex.Upsert)
-	a.Handle("GET", "/1.0/regex/:name", handlers.Regex.Retrieve)
-	a.Handle("DELETE", "/1.0/regex/:name", handlers.Regex.Delete)
+	a.Handle("GET", "/v1/regex", handlers.Regex.List)
+	a.Handle("PUT", "/v1/regex", handlers.Regex.Upsert)
+	a.Handle("GET", "/v1/regex/:name", handlers.Regex.Retrieve)
+	a.Handle("DELETE", "/v1/regex/:name", handlers.Regex.Delete)
 
-	a.Handle("GET", "/1.0/mask", handlers.Mask.List)
-	a.Handle("PUT", "/1.0/mask", handlers.Mask.Upsert)
-	a.Handle("GET", "/1.0/mask/:collection/:field", handlers.Mask.Retrieve)
-	a.Handle("GET", "/1.0/mask/:collection", handlers.Mask.Retrieve)
-	a.Handle("DELETE", "/1.0/mask/:collection/:field", handlers.Mask.Delete)
+	a.Handle("GET", "/v1/mask", handlers.Mask.List)
+	a.Handle("PUT", "/v1/mask", handlers.Mask.Upsert)
+	a.Handle("GET", "/v1/mask/:collection/:field", handlers.Mask.Retrieve)
+	a.Handle("GET", "/v1/mask/:collection", handlers.Mask.Retrieve)
+	a.Handle("DELETE", "/v1/mask/:collection/:field", handlers.Mask.Delete)
 
-	a.Handle("POST", "/1.0/exec", handlers.Exec.Custom)
-	a.Handle("GET", "/1.0/exec/:name", handlers.Exec.Name)
+	a.Handle("POST", "/v1/exec", handlers.Exec.Custom)
+	a.Handle("GET", "/v1/exec/:name", handlers.Exec.Name)
 
-	a.Handle("GET", "/1.0/relationship", handlers.Relationship.List)
-	a.Handle("PUT", "/1.0/relationship", handlers.Relationship.Upsert)
-	a.Handle("GET", "/1.0/relationship/:predicate", handlers.Relationship.Retrieve)
-	a.Handle("DELETE", "/1.0/relationship/:predicate", handlers.Relationship.Delete)
+	a.Handle("GET", "/v1/relationship", handlers.Relationship.List)
+	a.Handle("PUT", "/v1/relationship", handlers.Relationship.Upsert)
+	a.Handle("GET", "/v1/relationship/:predicate", handlers.Relationship.Retrieve)
+	a.Handle("DELETE", "/v1/relationship/:predicate", handlers.Relationship.Delete)
 
-	a.Handle("GET", "/1.0/view", handlers.View.List)
-	a.Handle("PUT", "/1.0/view", handlers.View.Upsert)
-	a.Handle("GET", "/1.0/view/:name", handlers.View.Retrieve)
-	a.Handle("DELETE", "/1.0/view/:name", handlers.View.Delete)
+	a.Handle("GET", "/v1/view", handlers.View.List)
+	a.Handle("PUT", "/v1/view", handlers.View.Upsert)
+	a.Handle("GET", "/v1/view/:name", handlers.View.Retrieve)
+	a.Handle("DELETE", "/v1/view/:name", handlers.View.Delete)
 
-	a.Handle("GET", "/1.0/pattern", handlers.Pattern.List)
-	a.Handle("PUT", "/1.0/pattern", handlers.Pattern.Upsert)
-	a.Handle("GET", "/1.0/pattern/:type", handlers.Pattern.Retrieve)
-	a.Handle("DELETE", "/1.0/pattern/:type", handlers.Pattern.Delete)
+	a.Handle("GET", "/v1/pattern", handlers.Pattern.List)
+	a.Handle("PUT", "/v1/pattern", handlers.Pattern.Upsert)
+	a.Handle("GET", "/v1/pattern/:type", handlers.Pattern.Retrieve)
+	a.Handle("DELETE", "/v1/pattern/:type", handlers.Pattern.Delete)
 
 }
 
