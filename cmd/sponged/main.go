7a8b6a62dce5ab8a5f61379fddd40beea10c4f79
gaba <gabelula@gmail.com>
2016-09-26 10:53:32 -0700

    All legal now..

--- cmd/sponged/main.go
+++ cmd/sponged/main.go
@@ -48,7 +48,7 @@ func main() {
 		writeTimeout = 30 * time.Second
 	)
 
-	if err := app.Run(":", routes.API(), readTimeout, writeTimeout); err != nil {
+	if err := app.Run(":4001", routes.API(), readTimeout, writeTimeout); err != nil {
 		log.Error("shutdown", "Init", err, "App Shutdown")
 		os.Exit(1)
 	}
