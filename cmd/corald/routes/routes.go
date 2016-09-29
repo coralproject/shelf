c3f35def455a0c4ebef255b50a5d56eb09b28703
gaba <gabelula@gmail.com>
2016-09-29 13:26:41 -0700

    Proxy Xenia endpoints that FE needs without quering views.

--- cmd/corald/routes/routes.go
+++ cmd/corald/routes/routes.go
@@ -112,6 +112,12 @@ func routes(a *app.App) {
 				return "/v1/exec/" + c.Params["query_set"]
 			}))
 
+	// Execute xenia queries directly
+	a.Handle("GET", "/v1/exec/:query_set", handlers.Proxy(xeniadURL, nil))
+
+	// Post a new query to xenia
+	a.Handle("PUT", "/v1/query", handlers.Proxy(xeniadURL, nil))
+
 	a.Handle("PUT", "/v1/item",
 		handlers.Proxy(spongedURL,
 			func(c *app.Context) string {
