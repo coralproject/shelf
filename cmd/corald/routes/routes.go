059bbae446f39e47552ff01b61b91a5ce32fb229
gaba <gabelula@gmail.com>
2016-10-12 09:40:22 -0700

    Add routes for actions.

--- cmd/corald/routes/routes.go
+++ cmd/corald/routes/routes.go
@@ -144,6 +144,11 @@ func routes(w *web.Web) {
 	w.Handle("POST", "/v1/exec",
 		handlers.Proxy(xeniadURL, func(c *web.Context) string { return "/v1/exec" }))
 
+	// Actions
+	w.Handle("POST", "/v1/action/:action/on/item/:item_key", handlers.Action.Create)
+	w.Handle("DELETE", "/v1/action/:action/on/item/:item_key", handlers.Action.Remove)
+
+	// Save or Update Items
 	w.Handle("PUT", "/v1/item",
 		handlers.Proxy(spongedURL, func(c *web.Context) string { return "/v1/item" }))
 
