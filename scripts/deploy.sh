9ef4f116b0557151d02e4069f8f419dcb4e64662
Wyatt Johnson <wyattjoh@gmail.com>
2016-10-14 15:45:41 -0600

    Standardized deploy script for dockerhub

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,19 +7,11 @@
 #
 # - BUILD_NUMNER
 # - GIT_COMMIT
-# - AWS_DEFAULT_REGION
-# - REPO_URL
 #
 
 # Based on an amazing article: http://dev.mikamai.com/post/144501132499/continuous-delivery-with-travis-and-ecs
 
 set -v
-
-# This is needed to login on AWS and push the image on ECR
-# Change it accordingly to your docker repo
-pip install --user awscli
-export PATH=$PATH:$HOME/.local/bin
-eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
 
 CURRENT_DATE=$(date +%Y-%m-%d)
 APPS=(askd corald sponged xeniad)
@@ -29,7 +21,6 @@ APPS=(askd corald sponged xeniad)
 for APP in "${APPS[@]}"
 do
   IMAGE_NAME=coralproject/$APP
-  REMOTE_IMAGE_URL="$REPO_URL/$IMAGE_NAME"
 
   echo "Building the $APP binary"
   GOOS=linux CGO_ENABLED=0 go build -a -ldflags "-X main.GitVersion=$BUILD_NUMNER -X main.GitRevision=$GIT_COMMIT -X main.BuildDate=$CURRENT_DATE" -o cmd/$APP/$APP github.com/coralproject/shelf/cmd/$APP
@@ -37,8 +28,9 @@ do
   echo "Build the $APP docker container"
   docker build -t $IMAGE_NAME cmd/$APP/
 
+  docker tag $IMAGE_NAME:latest
+
   echo "Pushing $IMAGE_NAME:latest"
-  docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
-  docker push "$REMOTE_IMAGE_URL:latest"
+  docker push $IMAGE_NAME:latest
   echo "Pushed $IMAGE_NAME:latest"
 done
